664985928f582bd95a1f4ea770eede013e6ad4c5
swapnilahmedshishir <ahmed66882298@gmail.com>
2026-07-06 21:45:27 +0600

    feat: added the env configration

---
+++ src/config/env.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const requiredEnvVariables = [
+  'NODE_ENV',
+  'PORT',
+  'DATABASE_URL',
+  'JWT_SECRET',
+  'JWT_EXPIRES_IN',
+  'BCRYPT_SALT_ROUNDS',
+  'CLIENT_URL',
+] as const;
+
+requiredEnvVariables.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+});
+
+export const env = {
+  NODE_ENV: process.env.NODE_ENV!,
+  PORT: Number(process.env.PORT),
+
+  DATABASE_URL: process.env.DATABASE_URL!,
+
+  JWT_SECRET: process.env.JWT_SECRET!,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN!,
+
+  BCRYPT_SALT_ROUNDS: Number(process.env.BCRYPT_SALT_ROUNDS),
+
+  CLIENT_URL: process.env.CLIENT_URL!,
+
+  CLOUDINARY: {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || '',
+    API_KEY: process.env.CLOUDINARY_API_KEY || '',
+    API_SECRET: process.env.CLOUDINARY_API_SECRET || '',
+  },
+} as const;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app';
+import { env } from './config/env';
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = env.PORT || 5000;
 
 async function main() {
   try {
     // MongoDB Connection
-    await mongoose.connect(process.env.DATABASE_URL as string);
+    await mongoose.connect(env.DATABASE_URL as string);
     console.log('📦 Database connected successfully!');
 
     app.listen(port, () => {
